910e956c0f4e36d21d628296cf7884df976cdfd6
Gustavo Cid Ornelas <gustavocidornelas@gmail.com>
2017-03-27 13:14:32 -0300

    "Removed the code that plotted the filter in the time and frequency domains. Removed the code related to the first (wrong) implementation of the filter."

--- src/+modem/@FiveGBlockModulation/fofdmFilter.m
+++ src/+modem/@FiveGBlockModulation/fofdmFilter.m
@@ -1,16 +1,12 @@
 function [filterInTime] = fofdmFilter (this, filterSize)
 
 % 5G bandwidth
-B_fiveG = (this.subcarrierSpacing)*(this.usefulSubcarriers + 1);
-f_low = -B_fiveG/2;
-f_high = B_fiveG/2;
-fs = (this.samplingRate); % Teste
+B_fiveG = (this.subcarrierSpacing)*(this.usefulSubcarriers + 1); % 5G bandwidth
+fs = (this.samplingRate); % Sampling frequency
 
 % Filter prototype in the frequency domain
-f = [0:(fs + 1)/filterSize:fs]; % Teste
-prototypeInFrequency = rectangularPulse(0, B_fiveG/2, f) + rectangularPulse(fs - B_fiveG/2, fs, f); %
-%f = [-2*B_fiveG:(4*B_fiveG + 1)/filterSize:2*B_fiveG];
-%prototypeInFrequency = rectangularPulse(f_low, f_high, f); % Rectangular pulse from -B_fiveG to B_fiveG
+f = [0:(fs + 1)/filterSize:fs]; 
+prototypeInFrequency = rectangularPulse(0, B_fiveG/2, f) + rectangularPulse(fs - B_fiveG/2, fs, f); % Rectangular pulses from 0 to B_fiveG/2 and from (B_fiveG/2-fs) to fs
 
 % Filter prototype in the time domain
 prototypeInTime = ifft(prototypeInFrequency); 
@@ -23,10 +19,3 @@ filterInTime = prototypeInTime.*window;
 % Normalizing the filter 
 [sum, ~] = sumsqr(filterInTime); % sum is the squared sum of all of filterInTime's coefficients
 filterInTime = (filterInTime)./sqrt(sum); % After normalizing, the squared sum of filterInTime's coefficients is equal to 1
-
-% Testing
-%plot(f, prototypeInFrequency);
-%figure();
-%plot(f, prototypeInTime);
-%figure();
-%stem(f, filterInTime);
