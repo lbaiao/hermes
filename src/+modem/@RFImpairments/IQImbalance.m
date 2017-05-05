84a47dd2ab7b5c7c47e5693e21acfd0e3873f319
lucasbaiao <lucasbaiao@live.com.pt>
2017-05-04 22:10:08 -0300

    IQImbalance modifications

--- src/+modem/@RFImpairments/IQImbalance.m
+++ src/+modem/@RFImpairments/IQImbalance.m
@@ -1,15 +1,12 @@
-function [ output_signal ] = IQImbalance( input_signal, a, phase, fc )
+function [ output_signal ] = IQImbalance( input_signal, a, phase)
 %IQIMBALANCE Summary of this function goes here
 %   input_signal = input signal
 %   a = amplitude increase due to the IQ Imbalance
 %   phase = phase increase due to the IQ Imabalance
-%   fc = Carrier frequency <----- verificar onde é feita a modulação para
-%   alta frequência
 
 qSignal = real(input_signal);
 iSignal = imag(input_signal);
-t = 0:1:length(input_signal); % a ser removido
-output_signal = qSignal*cos(2*pi*fc*t)-iSignal*(1+a)*sin(2*pi*fc*t + phase);
+output_signal = qSignal-iSignal*(1+a)*exp(1j*phase);
 
 end
 
